9ee10d5ff87d684b64d1a91796dc1749e0842f02
DCRG18200 <dicriroga18200@gmail.com>
2020-03-28 23:04:29 -0500

    Version Final

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,29 @@
 module.exports = function(grunt) {
+	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: 'js/scripts.js',
+				dest: 'js/scripts.min.js'
+			}
+		},
 		cssmin: {
 			css: {
-				src: 'css/main.css',
-				dest: 'css/main.min.css'
+				src: 'css/styles.css',
+				dest: 'css/styles.min.css'
 			}
 		}
 	});
 
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	grunt.registerTask('default', [ 'uglify' ]);
+
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-}
+};
